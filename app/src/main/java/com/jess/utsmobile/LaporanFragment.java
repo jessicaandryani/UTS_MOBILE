6b25a8566f8bc5da665ad294ecd23c223340469d
Syhri <fitraabadi12@gmail.com>
2024-12-19 01:04:12 +0800

    so malas

--- app/src/main/java/com/jess/utsmobile/LaporanFragment.java
+++ app/src/main/java/com/jess/utsmobile/LaporanFragment.java
@@ -8,44 +8,17 @@ import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import com.github.mikephil.charting.charts.PieChart;
+
 public class LaporanFragment extends Fragment {
 
-    private TextView cleanAssetsValue; // Untuk menampilkan Aset Bersih
-    private TextView accountBalance;   // Untuk menampilkan saldo akun
-    private DatabaseHelper dbHelper;
+    PieChart pieChart;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_laporan, container, false);
 
-        // Inisialisasi UI elements
-        cleanAssetsValue = view.findViewById(R.id.clean_assets_value);   // Menemukan clean_assets_value TextView
-        accountBalance = view.findViewById(R.id.account_balance);        // Menemukan account_balance TextView
-
-        // Inisialisasi DatabaseHelper
-        dbHelper = new DatabaseHelper(getContext());
-
-        // Mengambil dan menampilkan total pemasukan (Aset Bersih)
-        displayAsetBersih();
 
         return view;
-    }
-
-    // Fungsi untuk menampilkan Aset Bersih
-    private void displayAsetBersih() {
-        // Mengambil total pemasukan dan total pengeluaran dari database
-        double totalPemasukan = dbHelper.getTotalPemasukan();
-        double totalPengeluaran = dbHelper.getTotalPengeluaran();
-
-        // Menghitung Aset Bersih (total pemasukan - total pengeluaran)
-        double asetBersih = totalPemasukan - totalPengeluaran;
-
-        // Menampilkan Aset Bersih di UI
-        cleanAssetsValue.setText("Rp " + String.format("%,.0f", asetBersih));
-
-        // Menampilkan saldo akun (misalnya saldo = pemasukan - pengeluaran)
-        double saldoAkun = totalPemasukan - totalPengeluaran;
-        accountBalance.setText("Rp " + String.format("%,.0f", saldoAkun));
     }
 }
