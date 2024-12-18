d7fb3cf0319e41e985c28d5f79d00db98f6bf602
Syhri <fitraabadi12@gmail.com>
2024-12-18 13:35:17 +0800

    add changes at navbar top

--- app/src/main/java/com/jess/utsmobile/MainActivity.java
+++ app/src/main/java/com/jess/utsmobile/MainActivity.java
@@ -2,16 +2,15 @@ package com.jess.utsmobile;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import com.jess.utsmobile.databinding.ActivityMainBinding;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -47,14 +46,44 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
         binding.navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        if(savedInstanceState == null) {
-
+        if (savedInstanceState == null) {
             binding.navigation.setSelectedItemId(R.id.navigation_home);
         }
+
+        ImageView notificationBell = findViewById(R.id.notification_bell);
+        notificationBell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupMenu(v);
+            }
+        });
     }
+
+    private void showPopupMenu(View v) {
+        PopupMenu popupMenu = new PopupMenu(this, v);
+        popupMenu.getMenuInflater().inflate(R.menu.popup_menu_notifikasi, popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.notif1) {
+                    Toast.makeText(MainActivity.this, "Notifikasi 1 dipilih", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (item.getItemId() == R.id.notif2) {
+                    Toast.makeText(MainActivity.this, "Notifikasi 2 dipilih", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (item.getItemId() == R.id.notif3) {
+                    Toast.makeText(MainActivity.this, "Notifikasi 3 dipilih", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+        popupMenu.show();
+    }
+
     private void switchFragment(Fragment fragment) {
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.container_layout, fragment,
